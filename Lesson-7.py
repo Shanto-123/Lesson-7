3dd5b8140da381c1fdfd406a183a3d64cd6f85f2
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-25 02:37:39 +0600

    Add files via upload

---
+++ Lesson-7.py
@@ -0,0 +1,16 @@
+course = 'Learn python with Shanto'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('python'))
+print(course.replace('Shanto','programing'))
+print('python' in course)
+print(course.find('Programing'))
+print('Shanto' in course)
+
+
+
+
+
+
